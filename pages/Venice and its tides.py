b27f1479836eca22d4983b59ebce6edd7f7a1e5c
erikakorb <erika.korb99@gmail.com>
2023-01-30 11:55:08 +0100

    testing

--- pages/Venice and its tides.py
+++ pages/Venice and its tides.py
@@ -212,12 +212,13 @@ with col4:
     df = data.loc[data['Station'] == 'Pellestrina']
     df = WindConvert(df,'WindDir')
     df['newcol'] = df.index
+    df = df.rename({'WindVel':'km/h'}, axis=1) 
     fig = px.scatter_polar(df, r="newcol", theta="WindDir",
-                       color="WindVel", color_discrete_sequence=px.colors.sequential.YlOrRd, hover_data=[df.Hour, df.WindDir, df.WindVel])
+                       color="km/h", color_discrete_sequence=px.colors.sequential.GnBu, hover_data=[df.Data, df.WindDir, df['km/h']])
     fig.update_layout(showlegend = False,    polar = dict(
         radialaxis = dict(tickvals = [72,144,216], ticktext = ['-18h','-12h','-6h']) ,
         angularaxis = dict(tickvals = [0,45,90,135,180,225,270,315], ticktext = ['N','NE','E','SE','S','SW','W','NW'])    )   )
-    fig.update_traces(hovertemplate='Time=%{customdata[0]} <br> Direction=%{customdata[1]}° <br> Velocity=%{customdata[2]} km/h')
+    fig.update_traces(hovertemplate='%{customdata[0]} <br> Direction = %{customdata[1]}° <br> Velocity = %{customdata[2]:.2f} km/h')
     st.plotly_chart(fig, theme=None)
 
 
