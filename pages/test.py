b62ae2194b29123b677f35806a9694195276ea70
erikakorb <erika.korb99@gmail.com>
2023-01-30 09:26:04 +0100

    testing

---
+++ pages/test.py
@@ -0,0 +1,14 @@
+import plotly.express as px
+import matplotlib.pyplot as plt
+import streamlit as st
+##df = px.data.wind()
+##fig = px.scatter_polar(df, r="frequency", theta="direction",
+##                       color="strength", symbol="strength", size="frequency",
+##                       color_discrete_sequence=px.colors.sequential.Plasma_r)
+##st.pyplot(fig)
+
+#create your figure and get the figure object returned
+df = px.data.election()
+fig = px.scatter_ternary(df, a="Joly", b="Coderre", c="Bergeron")
+
+st.plotly_chart(fig, theme="streamlit")
