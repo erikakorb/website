a5a0dcb360ffcedcc06fa5333c299d779ada9f1a
erikakorb <erika.korb99@gmail.com>
2023-01-30 12:41:54 +0100

    testing

--- pages/Venice and its tides.py
+++ pages/Venice and its tides.py
@@ -201,17 +201,17 @@ with col2:
     st.write('### Venice')
     #st.dataframe(styler_water,width=500, height=40)
     #st.dataframe(last_wind,width=340, height=108)
-    st.metric(label="Water", value=str(last_water['Venice'][0]) + ' cm')
+    st.metric(label="Water", value=str(last_water.T['Venice'][0]) + ' cm')
 with col3:
     st.write('### Chioggia')
-    st.metric(label="Water", value=str(last_water['Chioggia'][0]) + ' cm')
+    st.metric(label="Water", value=str(last_water.T['Chioggia'][0]) + ' cm')
 with col4:
     st.write('### San Nicolò')
-    st.metric(label="Water", value=str(last_water['San Nicolò'][0]) + ' cm')
+    st.metric(label="Water", value=str(last_water.T['San Nicolò'][0]) + ' cm')
     st.metric(label="Velocity", value=str(last_wind['San Nicolò'][0]) + ' km/h')
 with col5:
     st.write('### Pellestrina')
-    st.metric(label="Water", value=str(last_water['Pellestrina'][0]) + ' cm')
+    st.metric(label="Water", value=str(last_water.T['Pellestrina'][0]) + ' cm')
     st.metric(label="Velocity", value=str(last_wind['Pellestrina'][0]) + ' km/h')
 
 def PolarPlot(NameStation):
