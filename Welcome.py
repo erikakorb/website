61df13e5622336fa7e16026f4319ff6fbb4ff84f
erikakorb <100794050+erikakorb@users.noreply.github.com>
2023-07-02 17:34:26 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -34,12 +34,35 @@ st.set_page_config(
 #         unsafe_allow_html=True,
 #     )
 
+# def add_logo():
+#     st.markdown(
+#         """
+#         <style>
+#             [data-testid="stSidebarNav"] {
+#                 background-image: url(https://github.com/erikakorb/website/blob/main/Korb_Erika_pic.jpg);
+#                 background-repeat: no-repeat;
+#                 padding-top: 120px;
+#                 background-position: 20px 20px;
+#             }
+#             [data-testid="stSidebarNav"]::before {
+#                 content: "My Company Name";
+#                 margin-left: 20px;
+#                 margin-top: 20px;
+#                 font-size: 30px;
+#                 position: relative;
+#                 top: 100px;
+#             }
+#         </style>
+#         """,
+#         unsafe_allow_html=True,
+#     )
+
 def add_logo():
     st.markdown(
         """
         <style>
             [data-testid="stSidebarNav"] {
-                background-image: url(https://github.com/erikakorb/website/blob/main/Korb_Erika_pic.jpg);
+                background-image: url(http://placekitten.com/200/200);
                 background-repeat: no-repeat;
                 padding-top: 120px;
                 background-position: 20px 20px;
