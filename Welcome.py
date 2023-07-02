fdf55576dfe6432ddbb85fd7634353ef56d4b210
erikakorb <100794050+erikakorb@users.noreply.github.com>
2023-07-02 17:08:49 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -12,7 +12,7 @@ st.write(
     """
     ##### Hi! I'm **Erika Korb**, a PhD student in Astrophysics at the University of Padua. """)
     
-st.write( """  ###### At work, I play with stars and binaries by means of numerical simulations in the [DemoBlack](https://demoblack.com/) group. In my free time, I play with codes as well. Ah, and I enjoy wandering around my hometown: Venice. """)
+st.write( """  ###### At work, I play with stars and binaries by means of numerical simulations in the [DemoBlack](https://demoblack.com/) group. When I am not in front of a computer, I am likely biking somewhere and taking pictures of the wonders I bump into. Ah, and I enjoy wandering around my hometown: Venice. """)
     
 st.write(
     """  ######  If you are curious to know more on my background, take a look at my [CV](https://github.com/erikakorb/CV/blob/main/CV.pdf).  If you are here for the fun and you want to see some fancy astrophysical project or a nice dashboard on Venice high tides, feel free to browse the sidebar. """)
