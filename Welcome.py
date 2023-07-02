be72c696e6011a8db1dc23375a7ab0c1aeb1f392
erikakorb <100794050+erikakorb@users.noreply.github.com>
2023-07-02 17:55:50 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -16,7 +16,7 @@ buffer = io.BytesIO()
 file.close()
 img_data = base64.b64decode(img_str)
 img = Image.open(io.BytesIO(img_data))
-resized_img = img.resize((80, 80))  # x, y
+resized_img = img.resize((100, ))  # x, y
 resized_img.save(buffer, format="PNG")
 img_b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
 
@@ -26,14 +26,14 @@ st.markdown(
             [data-testid="stSidebarNav"] {{
                 background-image: url('data:image/png;base64,{img_b64}');
                 background-repeat: no-repeat;
-                padding-top: 40px;
-                background-position: 120px 40px;
+                padding-top: 50px;
+                background-position: 100px 50px;
             }}
             [data-testid="stSidebarNav"]::before {{
                 content: " ";
                 margin-left: 20px;
                 margin-top: 20px;
-                font-size: 30px;
+                font-size: 50px;
                 position: relative;
                 top: 100px;
             }}
